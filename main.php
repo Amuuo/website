fdb56cc8c9ce3e7db76d51ec775233d878ab0647
Adam <adamthewilliams@gmail.com>
2019-02-02 10:51:53 -0600

    hello

---
+++ main.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" type="text/css" href="main.css">
+<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+<body bgcolor="#dddddd">                            
+    <table align="center">
+        <tr>
+            <td align="center" class="banner">
+                <p>WELCOME</p>
+            </td>
+        </tr>
+        <tr>
+            <td align="center">
+                <video class="test_shadow" autoplay muted loop>
+                    <source src="hello.mp4">
+                </video>                                                                            
+            </td>
+        </tr>
+        <tr>
+            <td align="center" 
+                style="height: 100px;">
+                <p></p>
+            </td>
+        </tr>                                          
+        <tr>
+            <td align="center">                       
+                    <p>
+                        <a align="center" style="font-color: black;" href="prog_anim.html">Programming</a>
+                        <a align="center" style="font-color: black;" href="alt_main.html">Alt_Main</a>                            
+                    </p>
+            </td>
+        </tr>                
+    </table>            
+</div>
+
+</body>
+</html>
