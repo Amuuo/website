d9d065ff8140846ff82544e942b2e2c60d2d1589
Adam <adamthewilliams@gmail.com>
2019-02-02 00:31:01 -0600

    first

--- main.js
+++ main.js
@@ -4,9 +4,10 @@ function fade(element) {
         if (op >= 1) clearInterval(timer);
         element.style.opacity = op;
         element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op += op * 0.2 || 0.2;
+        op += op * 0.1 || 0.1;
     }, 150);
 }
 var back = document.getElementById("videoHolder");
+back.getAnimations
 back.style.opacity = 0;
 fade(back);
