e4e1496a1c39fbfc9a7eb03bdb3049d0aed454b8
Adam <adamthewilliams@gmail.com>
2019-02-02 20:34:30 -0600

    css animation testing

--- main.php
+++ main.php
@@ -6,14 +6,28 @@
 <body bgcolor="#dddddd">                            
     <table align="center">
         <tr>
-            <td align="center" class="banner">
+            <td align="center" class="banner" id="title">
                 <p>WELCOME</p>
             </td>
         </tr>
         <tr>
             <td align="center">
-                <video class="test_shadow" autoplay muted loop>
-                    <source src="hello.mp4">
+                <video class="test_shadow" id="vid" autoplay muted loop>
+                    <source src="Comp 1_5.mp4">
+                </video>                                                                            
+            </td>
+        </tr>
+        <tr>
+            <td align="center">
+                <video class="test_shadow2" id="vid" autoplay muted loop>
+                    <source src="Comp 1_6.mp4">
+                </video>                                                                            
+            </td>
+        </tr>
+        <tr>
+            <td align="center">
+                <video class="test_shadow3" id="vid" autoplay muted loop>
+                    <source src="Comp 1_8.mp4">
                 </video>                                                                            
             </td>
         </tr>
@@ -25,10 +39,10 @@
         </tr>                                          
         <tr>
             <td align="center">                       
-                    <p>
-                        <a align="center" style="font-color: black;" href="prog_anim.html">Programming</a>
-                        <a align="center" style="font-color: black;" href="alt_main.html">Alt_Main</a>                            
-                    </p>
+                <p class="banner" id="links">
+                    <a align="center" style="font-color: black;" href="prog_anim.html">Programming</a>
+                    <a align="center" style="font-color: black;" href="alt_main.html">Alt_Main</a>                            
+                </p>
             </td>
         </tr>                
     </table>            
