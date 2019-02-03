6ce11ab8bf045d988ecc7dcd54517d0572a98bab
Adam <adamthewilliams@gmail.com>
2019-02-03 05:11:11 -0600

    working with dynamic box

--- sql_test.php
+++ sql_test.php
@@ -3,49 +3,25 @@
 <html>
 <link rel="stylesheet" type="text/css" href="main.css">
 <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-<body bgcolor="#dddddd">                            
-    <table align="center">
-        <tr>
-            <td align="center" class="banner" id="title">
-                <p>WELCOME</p>
-            </td>
-        </tr>
-        <tr>
-            <td align="center">
-                <video class="test_shadow" id="vid" loop preload="none">
-                    <source src="Comp 1_5.mp4">
-                </video>                                                                            
-            </td>
-        </tr>
-        <tr>
-            <td align="center">
-                <video class="test_shadow2" id="vid" loop preload="none">
-                    <source src="Comp 1_6.mp4">
-                </video>                                                                            
-            </td>
-        </tr>
-        <tr>
-            <td align="center">
-                <video class="test_shadow3" id="vid" loop preload="none">
-                    <source src="Comp 1_8.mp4">
-                </video>                                                                            
-            </td>
-        </tr>
-        <tr>
-            <td align="center" 
-                style="height: 100px;">
-                <p></p>
-            </td>
-        </tr>                                          
-        <tr>
-            <td align="center">                       
-                <p class="banner" id="links">
-                    <a align="center" style="font-color: black;" href="prog_anim.html">Programming</a>
-                    <a align="center" style="font-color: black;" href="alt_main.html">Alt_Main</a>                            
-                </p>
-            </td>
-        </tr>                
-    </table>            
+<body bgcolor="#dddddd">                             
+
+
+
+<div>
+    <form action="welcome.php" method="get|post">        
+    User:        <input type="text"     name="username"><br>
+    Password:    <input type="password" name="password"><br>
+    City:        <input type="text"     name="name"><br>
+    CountryCode: <input type="text"     name="code"><br>
+    District:    <input type="text"     name="district"><br>
+    Population:  <input type="text"     name="population">
+    <select name="operator">
+        <option value=">">></option>
+        <option value="=">=</option>
+        <option value="<"><</option>
+    </select>
+    <input type="submit">
+    </form>       
 </div>
 
 </body>
--- welcome.php
+++ welcome.php
@@ -1,29 +1,83 @@
 <html>
-    <body>
-        <div>
-            <p>
-            <?php        
-                $conn = new mysqli('localhost', 'adam', 'h495090911', 'world');
-                if ($conn->connect_error) {
-                    die("Connection failed: " . $conn->connect_error);
-                }
-                echo "Connected successfully";
-            ?>
-            </p>
-        </div>
-        <div>
-            $sql = "SELECT * City = City AND CountryCode = CountryCode";
-            $result = $conn->query($sql);
+<link rel="stylesheet" type="text/css" href="main.css">
+<body>
 
-            if ($result->num_rows > 0) {
-                while($row = $result->fetch_assoc()){
-                    echo "City: " . $row["City"]. " - CountryCode: " . $row["CountryCode"]. " <br>                    
-                }
+    <?php   
+        
+        $name       = $_GET["name"];
+        $code       = $_GET["code"];
+        $district   = $_GET["district"];
+        $population = $_GET["population"];
+        $operator   = $_GET["operator"];
+        $username   = $_GET["username"];
+        $password   = $_GET["password"];
+        
+        //echo "$name<br>";
+        //echo "$code<br>";
+        
+        $conn = new mysqli("localhost", $username, $password, "world");
+        
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        
+        //echo "Connected successfully<br>";
+
+        
+        unset($sql);
+        
+        if($name) {
+            $sql[] = "Name = '$name'";                        
+        }
+        if($_GET["district"]){
+            $sql[] = "District = '$district'";                        
+        }
+        if($_GET["code"]) {
+            $sql[] = "CountryCode = '$code'";                        
+        } 
+        if($_GET["population"]) {
+            $sql[] = "Population $operator $population";                        
+        }  
+        
+        $query = "SELECT * FROM city";
+        if(!empty($sql)){
+            $query .= ' WHERE ' . implode(' AND ', $sql);
+        }
+
+        //echo "$query";
+
+        
+        echo "<table><tbody>";
+        $result = $conn->query($query);
+
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {                
+                echo "<tr>";
+                echo utf8_encode("<td> " . $row["Name"] . "</td>");
+                echo utf8_encode("<td>" . $row["District"] . "</td>");
+                echo utf8_encode("<td>" . $row["CountryCode"] . "</td>");
+                echo utf8_encode("<td align='right'>" . number_format($row["Population"]) . "</td>");
+                echo "</tr>";
+                /*
+                $format = "<br>%s %s %s %s";
+                echo utf8_encode(sprintf($format, $row["Name"] , 
+                                                  $row["CountryCode"], 
+                                                  $row["District"], 
+                                                  $row["Population"]));
+                */
             }
-            else {
-                echo "0 results";
-            }
-            $conn->close();            
-        </div>
-    </body>
+        } else {                
+            echo "<br>0 results";
+        }
+        echo "</tbody>";
+        echo "<thead><tr>";
+        echo "<th>City</th>";
+        echo "<th>District</th>";
+        echo "<th>Country</th>";
+        echo "<th>Population</th>";
+        echo "</thead></tr></table>";
+        $conn->close(); 
+    ?>
+    
+</body>
 </html>
